c286d57bc3ba490d4410e7a243c124441eb3d2aa
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-13 22:38:28 -0800

    iniilized repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'; // Tailwind CSS import
+
+function App() {
+  return (
+    <div className="text-5xl text-center mt-5 border-red-900">
+      <h1 className='text-[red]'>
+        welcome to Typescript with  vite plus tailwand css
+      </h1>
+    </div>
+  );
+}
+
+export default App;
