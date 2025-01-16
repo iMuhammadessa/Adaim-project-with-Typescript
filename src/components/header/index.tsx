376dae2238ff0d5afb65bd3e75fea08e4617ebbe
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-16 06:07:58 -0800

    Added Header and Footer Component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -67,30 +67,35 @@ function Header() {
             className="bg-gray-800 bg-opacity-90 px-6 py-4 fixed top-0 left-0 right-0 z-50 md:hidden transform transition-transform duration-300 ease-in-out"
             id="mobile-menu"
           >
-            <Link to="/" className="block py-2 hover:text-gray-400">
-              Home
-            </Link>
-            <Link to="/about" className="block py-2 hover:text-gray-400">
-              About
-            </Link>
-            <Link to="/services" className="block py-2 hover:text-gray-400">
-              Services
-            </Link>
-            <Link to="/blog" className="block py-2 hover:text-gray-400">
-              Blog
-            </Link>
-            <Link to="/announcement" className="block py-2 hover:text-gray-400">
-              Announcement
-            </Link>
-            <Link to="/contact" className="block py-2 hover:text-gray-400">
-              Contact
-            </Link>
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-4 w-full rounded"
-              onClick={() => (window.location.href = "tel:+1234567890")}
-            >
-              Call Now
-            </button>
+            <div className="flex flex-col space-y-4">
+              <Link to="/" className="block py-2 hover:text-gray-400">
+                Home
+              </Link>
+              <Link to="/about" className="block py-2 hover:text-gray-400">
+                About
+              </Link>
+              <Link to="/services" className="block py-2 hover:text-gray-400">
+                Services
+              </Link>
+              <Link to="/blog" className="block py-2 hover:text-gray-400">
+                Blog
+              </Link>
+              <Link
+                to="/announcement"
+                className="block py-2 hover:text-gray-400"
+              >
+                Announcement
+              </Link>
+              <Link to="/contact" className="block py-2 hover:text-gray-400">
+                Contact
+              </Link>
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-4 w-full rounded"
+                onClick={() => (window.location.href = "tel:+1234567890")}
+              >
+                Call Now
+              </button>
+            </div>
           </nav>
         </div>
       </div>
