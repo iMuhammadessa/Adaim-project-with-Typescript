28321092b7cc1e3fec98336dd09d88fba30dfcbb
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-21 07:02:54 -0800

    changes has been completed

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,12 @@
 import HeroSection from "../../components/banner";
 
-function AboutPage() {
-  const sliderImages = [
-    "/images/home/containerpic1.jpg",
-    "/images/home/containerpic2.jpg",
-    "/images/home/containerpic3.jpg",
-  ];
+const sliderImages = [
+  "/images/home/containerpic1.jpg",
+  "/images/home/containerpic2.jpg",
+  "/images/home/containerpic3.jpg",
+];
 
+function AboutPage() {
   return (
     <HeroSection
       backgroundimage="/images/home/containerpic1.jpg"
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,12 @@
 import HeroSection from "../../components/banner";
 
-function HomePage() {
-  const sliderImages = [
-    "/images/home/containerpic1.jpg",
-    "/images/home/containerpic2.jpg",
-    "/images/home/containerpic3.jpg",
-  ];
+const sliderImages = [
+  "/images/home/containerpic1.jpg",
+  "/images/home/containerpic2.jpg",
+  "/images/home/containerpic3.jpg",
+];
 
+function HomePage() {
   return (
     <HeroSection
       backgroundimage="/images/home/containerpic1.jpg"
