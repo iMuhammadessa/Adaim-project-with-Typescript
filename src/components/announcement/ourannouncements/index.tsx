220e537e58bb7b9b4eaf03dc995a33a45fc8dca0
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-27 20:08:01 -0800

    changes saved

--- src/components/announcement/ourannouncements/index.tsx
+++ src/components/announcement/ourannouncements/index.tsx
@@ -1,10 +1,12 @@
 function OurAnnouncement() {
   return (
-    <div>
-      <div className="text-bold text-5xl mt-[5rem] text-center">
+    <div className="px-4 py-8 mx-auto max-w-screen-lg">
+      <div className="font-bold text-3xl sm:text-4xl md:text-5xl mt-16 text-center">
         Our Announcement
       </div>
-      <div className="text-center text-2xl mt-7">No Announcement available</div>
+      <div className="text-center text-lg sm:text-xl md:text-2xl mt-5">
+        No Announcement available
+      </div>
     </div>
   );
 }
