6c8c0cc40f6554dc426625008dd704338f674942
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-27 19:59:19 -0800

    our-announcement page designed

---
+++ src/components/announcement/ourannouncements/index.tsx
@@ -0,0 +1,12 @@
+function OurAnnouncement() {
+  return (
+    <div>
+      <div className="text-bold text-5xl mt-[5rem] text-center">
+        Our Announcement
+      </div>
+      <div className="text-center text-2xl mt-7">No Announcement available</div>
+    </div>
+  );
+}
+
+export default OurAnnouncement;
