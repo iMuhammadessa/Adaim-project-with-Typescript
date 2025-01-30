a46d0ad0e4d2322974e731b5027bcb312ded44bc
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-29 21:52:11 -0800

    service page added

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,5 +1,21 @@
+import HeroSection from "../../components/banner";
+
+const sliderImages = [
+  "/images/home/containerpic1.jpg",
+  "/images/home/containerpic2.jpg",
+  "/images/home/containerpic3.jpg",
+];
+
 function ServicesPage() {
-  return <div>Service page is here...</div>;
+  return (
+    <HeroSection
+      backgroundimage="/images/home/containerpic1.jpg"
+      title="Services"
+      description="We provide a wide range of services tailored to meet your specific needs. With a steadfast commitment to client satisfaction,
+                   AD AIM Marketing forges enduring partnerships and endeavors to surpass expectations."
+      sliderImages={sliderImages}
+    />
+  );
 }
 
 export default ServicesPage;
