1d3eea6dc0e0d3d4ebc0f43d9fb4114c55f9f506
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-02-04 23:38:19 -0800

    conflicts resolved

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,13 @@ import Login from "./pages/login";
 import Registration from "./pages/registration";
 
 function App() {
-  const Loginpage = window.location.pathname === "/login";
-  const RegistrationPage = window.location.pathname === "/Registration";
+  const loginPage = window.location.pathname === "/login";
+  const registrationPage = window.location.pathname === "/registration";
 
   return (
     <Router>
-      {!Loginpage && !RegistrationPage && <Header />}
+      {!loginPage && !registrationPage && <Header />}
 
-      {window.location.pathname !== "/Registration" && <Header />}
-
-
-
-function App() {
-  const Registrationpage = window.location.pathname === "/Registration";
-
-  return (
-    <Router>
-      {!Registrationpage && <Header />}
->>>>>>> c6d9cb311cb0c23b56fb0322b74af1ae867d8bee
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -36,17 +25,11 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/announcement" element={<Announcement />} />
         <Route path="/contact" element={<Contact />} />
-<<<<<<< HEAD
         <Route path="/login" element={<Login />} />
-        <Route path="/Registration" element={<Registration />} />
+        <Route path="/registration" element={<Registration />} />
       </Routes>
 
-      {!Loginpage && !RegistrationPage && <Footer />}
-=======
-        <Route path="/Registration" element={<Registration />} />
-      </Routes>
-      {!Registrationpage && <Footer />}
->>>>>>> c6d9cb311cb0c23b56fb0322b74af1ae867d8bee
+      {!loginPage && !registrationPage && <Footer />}
     </Router>
   );
 }
