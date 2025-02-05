bb1e74f1c2a7965dab865342613a2f5c703766df
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-02-05 00:05:05 -0800

    hide header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,14 @@ import Announcement from "./pages/announcement";
 import Contact from "./pages/contact";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import Login from "./pages/login";
 import Registration from "./pages/registration";
 
 function App() {
-  const loginPage = window.location.pathname === "/login";
   const registrationPage = window.location.pathname === "/registration";
 
   return (
     <Router>
-      {/* Check for login and registration pages */}
-      {!loginPage && !registrationPage && <Header />}
+      {registrationPage ? null : <Header />}
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -26,12 +23,10 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/announcement" element={<Announcement />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
       </Routes>
 
-      {/* Check for login and registration pages */}
-      {!loginPage && !registrationPage && <Footer />}
+      {registrationPage ? null : <Footer />}
     </Router>
   );
 }
