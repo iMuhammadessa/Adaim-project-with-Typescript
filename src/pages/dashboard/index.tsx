ef7a0c6ab88f33085c6ad85b7fb3e71c386ef430
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-03-04 12:28:15 -0800

    fixed ui in main

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router-dom";
 
 function MainDashboard() {
   return (
-    <div className="flex flex-col md:flex-row min-h-screen bg-white">
-      <DashboardNavigation />
-      <div className="flex-1 flex flex-col items-center justify-start pt-4 md:pt-8 p-6 md:pl-16">
+    <div className="flex min-h-screen bg-white">
+      <div className="fixed md:relative w-64 h-screen bg-gray-100">
+        <DashboardNavigation />
+      </div>
+
+      <div className="flex-1 flex flex-col items-center justify-start pt-4 md:pt-8 p-6 md:ml-64">
         <Outlet />
       </div>
     </div>
